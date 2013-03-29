8869a7ce3554929d035fba6a206e8e96bef37ba5
John Berry <jberry+githubcommits@lumoslabs.com>
2013-03-28 21:04:58 -0700

    Initialized Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alaskan::Application.config.secret_token = '8848e2a139c19e07b6abd0a17de4c4d5c5acb791fd50833513aa86f39ce01218abc0c660ddfc6d25d44e2944d8d8f351e66d0a760813aa721d29f26cc6ebbfc5'
